457119ef649b1b0f2675956a66b9d6ac7b21b0fe
teshager habtie <teshie00001@gmail.com>
2024-08-10 09:58:22 -0500

    this adding reset utton

--- src/components/Statehooks.js
+++ src/components/Statehooks.js
@@ -6,16 +6,25 @@ import React, { useState } from "react";
 function Statehooks() {
   //3/  Initialize state value by calling useState() at the top component  (use destructuring):
   const [count, setCount] = useState(0);
-  const [age, setage] = useState(10);
+  const [age, setage] = useState(0);
   const [fruit, setFruit] = useState("Banana");
+  console.log(count);
   const countUpdater = () => {
     //Update your state using the updater function
-    setCount((x) => x + 1);
-  };
+    setCount((count) => count + 1);
 
+    if (count % 2 != 0) {
+      setage((x) => x + 1);
+    }
+  };
+  const reserUpdater = () => {
+    setCount((x) => (x = 0));
+    setage((x) => (x = 0));
+  };
   return (
     <div>
       <button onClick={countUpdater}>ClickToIncreament </button>
+      <button onClick={reserUpdater}>Reseat all </button>
       <h3>Count displayer: {count}</h3>
       <h3>Count displayer: {age}</h3>
     </div>
